7664fe06555b1ecdd73f79c2a902140e1c8a889f
Layammieh <layamgracielle@gmail.com>
2026-02-19 09:33:12 +0800

    Folder structure and usersPage - fixed and created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/admin/auth/Login";
+import SignUp from "./pages/admin/auth/SignUp";
+import Dashboard from "./pages/admin/Dashboard";
+import UsersPage from "./pages/admin/Users";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Navigate to="/admin/dashboard" replace />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/users" element={<UsersPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/admin/auth/Login.jsx
+++ src/pages/admin/auth/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Mail, Lock, Eye, EyeOff } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/zapcycle_logo.png";
+import logo from "../../../assets/zapcycle_logo.png";
 
 const Login = () => {
   const navigate = useNavigate();
